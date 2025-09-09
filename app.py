d1bd3e75be9dbd55206f4d7c6f6c2d9aa02af45a
sekiei <sekieiwork@gmail.com>
2025-09-10 03:38:20 +0900

    adcmfix

--- app.py
+++ app.py
@@ -80,9 +80,10 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    image_filename = db.Column(db.String(100), nullable=True) # <-- この行を追加
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False) # <-- ここに追加
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationship('Comment', backref='post', lazy=True, cascade="all, delete")
     bookmarks = db.relationship(
         'Bookmark', 
@@ -91,9 +92,6 @@ class Post(db.Model):
         cascade="all, delete", 
     )
     notifications = db.relationship('Notification', backref='post', lazy='dynamic', cascade="all, delete")
-
-    def __repr__(self):
-        return f'<Post {self.id}>'
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
