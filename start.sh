366d269174f15bb4ca2d6f5851fcd2c42d8b3062
sekiei <sekieiwork@gmail.com>
2025-11-18 10:26:40 +0900

    Temporarily change start.sh to stamp DB revision

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# データベースのマイグレーション（テーブル作成）を実行する
-echo "Running database migrations..."
-flask db upgrade
+# データベースの履歴を強制的に最新版(abf...)にスタンプする
+echo "Stamping database to revision abf7081a2a16..."
+flask db stamp abf7081a2a16
 
 # Webサーバー（Gunicorn）を起動する
 echo "Starting Gunicorn server..."
