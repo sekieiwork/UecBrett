37dc58bc148e550c426944033c132c7d86e555ea
sekiei <sekieiwork@gmail.com>
2025-11-25 14:00:25 +0900

    sort

--- start.sh
+++ start.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# ▼▼▼ 修正版: DB履歴のリセット処理 ▼▼▼
-echo "Resetting alembic history..."
-python -c "
-from app import app, db
-from sqlalchemy import text
-
-with app.app_context():
-    with db.engine.connect() as conn:
-        conn.execute(text('DROP TABLE IF EXISTS alembic_version'))
-        conn.commit()
-"
-# ▲▲▲ ここまで修正 ▲▲▲
-
 # データベースのマイグレーション（テーブル作成）を実行する
 echo "Running database migrations..."
 flask db upgrade
